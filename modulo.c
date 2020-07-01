3d86ae54f876949a708a35c3b491465c89f6e1e2
Gonzalo E. Sanchez <gonzalo.e.sanchez@hotmail.com>
2020-07-01 19:04:53 -0300

    Modulo hola mundo
    Muestra las funcionalidades de las secciones __init y __exit

--- modulo.c
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/i2c.h>
-
-/* Modulo trivial, solo para test si compila correctamente */
-
-MODULE_LICENSE("GPL");
-
